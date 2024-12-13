8c9a65032a408b0337cec449719840f12e2dcb9a
lakshya kalra <106165506+lakshya-klr@users.noreply.github.com>
2024-12-13 17:08:57 +0530

    Time: 500 ms (5.39%), Space: 61.4 MB (25.46%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.java
@@ -1,23 +1,24 @@
 class Solution {
     public long findScore(int[] nums) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->a[0]==b[0]?a[1]-b[1]:a[0]-b[0]);
-    
-    for(int i =0;i<nums.length;i++){
-        pq.offer(new int[] {nums[i],i});
-    }
-    Set<Integer> set = new HashSet<>();
-    long score=0;
-    while(!pq.isEmpty() && set.size()!=nums.length){
-        int[] top= pq.poll();
-        if(set.contains(top[1])){
-            continue;
+        PriorityQueue<int[]> pq = new PriorityQueue<>(new Comparator<>(){
+            public int compare(int[] a, int[] b){
+                if(a[0]<b[0]) return -1;
+                else if(a[0]>b[0]) return 1;
+                return Integer.compare(a[1],b[1]);
+            }
+        });
+        Set<Integer> marked = new HashSet<>();
+        for(int i =0;i<nums.length;i++) pq.add(new int[]{nums[i],i});
+        long score=0;
+        while(!pq.isEmpty()){
+            while(!pq.isEmpty() && marked.contains(pq.peek()[1])) pq.remove();
+            if(pq.isEmpty()) break;
+            int current[] = pq.remove();
+            score+=current[0];
+            marked.add(current[1]);
+            if(current[1]-1>=0) marked.add(current[1]-1);
+            if(current[1]+1<nums.length) marked.add(current[1]+1);
         }
-        score+=top[0];
-        if(top[1]-1>=0) set.add(top[1]-1);
-        if(top[1]+1<nums.length) set.add(top[1]+1);
+        return score;
     }
-    return score;
-    }
-
-
 }
